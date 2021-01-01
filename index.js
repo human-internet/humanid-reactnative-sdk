edff054577cda0b3ba3d607fb0aa08cb1e9c0285
bobbyecho <raden.bambang@ralali.com>
2021-01-02 05:56:03 +0700

    chore: update ios example

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ import AppLogo from './AppLogo';
 
 configureHumanID({
   appName: 'YOUR_APP_NAME',
-  clientSecret: 'APP_SECRET',
-  clientId: 'APP_ID',
+  clientSecret:
+    '12FZ3llRg5KFDuJFLftOxlQof1DKBtgL7mZrY4AE1zaM78o1Fvza2IZdKjdxT45Q',
+  clientId: 'MOBILE_m65nshUmT9BDchwFEKdz',
   Icon: AppLogo,
 });
 
