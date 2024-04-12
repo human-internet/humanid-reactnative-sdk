c638d599d97d41537e7c5a4c22ce2ec2cb9fb8df
ottoh hidayatullah <ottoh.hidayatullah@ai.astra.co.id>
2024-04-12 22:53:29 +0700

    chore : remove and added folder react native humanid

--- src/modules/Login/CountryPicker/FlatListSeparator.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {View} from 'react-native';
-import styles from '../styles';
-
-function FlatListSeparator(): JSX.Element {
-  return <View style={styles.separator} />;
-}
-
-export default FlatListSeparator;
--- src/modules/Toast/helpers.ts
+++
@@ -1,9 +0,0 @@
-import {Platform} from 'react-native';
-
-export function convertDuration(duration: number): number {
-    if (Platform.OS === 'ios') {
-        return duration / 1000;
-    }
-
-    return duration;
-}
